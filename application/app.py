06bddf88425b23dfaf93e13614806b8f1996db84
zainshabeeb <shabeeby8@gmail.com>
2026-04-20 17:25:52 -0400

    Update app.py

--- application/app.py
+++ application/app.py
@@ -348,12 +348,7 @@ class MaskApp:
 
         # Config defaults (user can change in Settings)
         self.model_checkpoint = str(self.APP_DIR.parent / "checkpoints" / "finetuned_sam2.1.pt")
-        # Resolve config from the installed sam2 package
-        try:
-            import sam2 as _sam2_pkg
-            self.model_cfg = str(Path(_sam2_pkg.__file__).parent / "configs" / "sam2.1" / "sam2.1_hiera_l.yaml")
-        except ImportError:
-            self.model_cfg = "../sam2/configs/sam2.1/sam2.1_hiera_l.yaml"
+        self.model_cfg = str(self.APP_DIR.parent / "sam2.1_hiera_l.yaml")
         self.point_color = 'red'
         self.box_color = 'green'
         # Experiment defaults
