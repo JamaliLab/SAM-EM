9b50a5d946ea408017ae1d86660d63abbdfb8229
siddarthsreeram <79339644+siddarthsreeram@users.noreply.github.com>
2026-06-15 20:21:40 -0700

    Update launch_app.sh

--- launch_app.sh
+++ launch_app.sh
@@ -79,9 +79,7 @@ if [ ! -d "$ENV_DIR" ]; then
 
     echo
     echo "Installing remaining dependencies..."
-    "$PIP_EXE" install numpy==2.3.3 pillow==11.3.0 matplotlib==3.10.6 scikit-image==0.25.2 pandas==2.3.3 \
-        hydra-core iopath omegaconf tqdm \
-        customtkinter CTkMessagebox CTkColorPicker
+    "$PIP_EXE" install numpy==2.2.6 pillow==12.2.0 matplotlib==3.10.8 scikit-image==0.25.2 pandas==2.3.3 hydra-core==1.3.2 iopath==0.1.10 omegaconf==2.3.0 tqdm==4.67.3 customtkinter==5.2.2 CTkMessagebox==2.7 CTkColorPicker==0.9.0
     if [ $? -ne 0 ]; then
         echo "ERROR: Failed to install dependencies."
         read -p "Press Enter to exit..."
